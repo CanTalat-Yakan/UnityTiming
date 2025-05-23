102c4453884b5fba2be07c5bb223bc856e4169b9
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-23 10:02:26 +0200

    ⠀⠀⠀⠀⠀

--- Tests/TimingTests.cs
+++ Tests/TimingTests.cs
@@ -4,163 +4,164 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.TestTools;
-using UnityEssentials;
 
-public class TimingTests
+namespace UnityEssentials.Tests
 {
-    [SetUp]
-    public void SetUp()
+    public class TimingTests
     {
-        // Ensure singleton is initialized and clean before each test
-        if (Timing.Instance == null)
+        [SetUp]
+        public void SetUp()
         {
-            var go = new GameObject("Timing");
-            go.AddComponent<Timing>();
-        }
-        Timing.KillAllCoroutines();
-    }
-
-    [TearDown]
-    public void TearDown()
-    {
-        Timing.KillAllCoroutines();
-    }
-
-    private IEnumerator<float> SimpleCoroutine()
-    {
-        yield return 0.1f;
-        yield return 0.2f;
-    }
-
-    [Test]
-    public void RunCoroutine_ShouldReturnValidHandle()
-    {
-        var handle = Timing.RunCoroutine(SimpleCoroutine());
-        Assert.IsTrue(handle.IsValid);
-    }
-
-    [Test]
-    public void IsCoroutineActive_ShouldReturnTrueForActiveCoroutine()
-    {
-        var handle = Timing.RunCoroutine(SimpleCoroutine());
-        Assert.IsTrue(Timing.IsCoroutineActive(handle.Version));
-    }
-
-    [Test]
-    public void IsCoroutineActive_ShouldReturnFalseForKilledCoroutine()
-    {
-        var handle = Timing.RunCoroutine(SimpleCoroutine());
-        Timing.KillCoroutine(handle.Version);
-        Assert.IsFalse(Timing.IsCoroutineActive(handle.Version));
-    }
-
-    [Test]
-    public void PauseCoroutine_ShouldPauseCoroutine()
-    {
-        var handle = Timing.RunCoroutine(SimpleCoroutine());
-        Timing.PauseCoroutine(handle.Version);
-
-        // Access internal state to verify paused (reflection or friend class if needed)
-        // Here, we check that the coroutine is still active but will not advance
-        Assert.IsTrue(Timing.IsCoroutineActive(handle.Version));
-    }
-
-    [Test]
-    public void ResumeCoroutine_ShouldResumePausedCoroutine()
-    {
-        var handle = Timing.RunCoroutine(SimpleCoroutine());
-        Timing.PauseCoroutine(handle.Version);
-        Timing.ResumeCoroutine(handle.Version);
-
-        Assert.IsTrue(Timing.IsCoroutineActive(handle.Version));
-    }
-
-    [Test]
-    public void KillCoroutine_ShouldRemoveCoroutine()
-    {
-        var handle = Timing.RunCoroutine(SimpleCoroutine());
-        Timing.KillCoroutine(handle.Version);
-        Assert.IsFalse(Timing.IsCoroutineActive(handle.Version));
-    }
-
-    [Test]
-    public void KillAllCoroutines_ShouldRemoveAllCoroutines()
-    {
-        var handle1 = Timing.RunCoroutine(SimpleCoroutine());
-        var handle2 = Timing.RunCoroutine(SimpleCoroutine(), Segment.FixedUpdate);
-
-        Timing.KillAllCoroutines();
-
-        Assert.IsFalse(Timing.IsCoroutineActive(handle1.Version));
-        Assert.IsFalse(Timing.IsCoroutineActive(handle2.Version));
-    }
-
-    [Test]
-    public void KillAllCoroutines_BySegment_ShouldRemoveOnlyThatSegment()
-    {
-        var handle1 = Timing.RunCoroutine(SimpleCoroutine(), Segment.Update);
-        var handle2 = Timing.RunCoroutine(SimpleCoroutine(), Segment.FixedUpdate);
-
-        Timing.KillAllCoroutines((int)Segment.Update);
-
-        Assert.IsFalse(Timing.IsCoroutineActive(handle1.Version));
-        Assert.IsTrue(Timing.IsCoroutineActive(handle2.Version));
-    }
-
-    [Test]
-    public void ProcessSegment_ShouldAdvanceCoroutine()
-    {
-        // Set up a coroutine that increments a counter
-        int counter = 0;
-        IEnumerator<float> CountingCoroutine()
-        {
-            counter++;
-            yield return 0;
-            counter++;
+            // Ensure singleton is initialized and clean before each test
+            if (Timing.Instance == null)
+            {
+                var go = new GameObject("Timing");
+                go.AddComponent<Timing>();
+            }
+            Timing.KillAllCoroutines();
         }
 
-        var handle = Timing.RunCoroutine(CountingCoroutine(), Segment.Update);
+        [TearDown]
+        public void TearDown()
+        {
+            Timing.KillAllCoroutines();
+        }
 
-        // Call Update (which calls ProcessSegment)
-        Timing.Instance.Update();
+        private IEnumerator<float> SimpleCoroutine()
+        {
+            yield return 0.1f;
+            yield return 0.2f;
+        }
 
-        Assert.AreEqual(2, counter); // Should have advanced both steps
-    }
-
-    [Test]
-    public void OnDestroy_ShouldKillAllCoroutines()
-    {
-        var handle = Timing.RunCoroutine(SimpleCoroutine());
-        Timing.Instance.OnDestroy();
-        Assert.IsFalse(Timing.IsCoroutineActive(handle.Version));
-    }
-
-    [Test]
-    public void Awake_ShouldInitializePools()
-    {
-        Timing.Instance.Awake();
-
-        var processPool = Timing.Instance.ProcessPool;
-        var handlePool = Timing.Instance.HandlePool;
-
-        Assert.IsNotNull(processPool);
-        Assert.IsNotNull(handlePool);
-    }
-
-    [UnityTest]
-    public IEnumerator ZeroAllocationCheck()
-    {
-        GC.Collect();
-        var allocBefore = UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong();
-
-        for (int i = 0; i < 100; i++)
+        [Test]
+        public void RunCoroutine_ShouldReturnValidHandle()
         {
             var handle = Timing.RunCoroutine(SimpleCoroutine());
-            yield return null;
-            //Timing.KillCoroutine(handle.Version);
+            Assert.IsTrue(handle.IsValid);
         }
 
-        var allocAfter = UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong();
-        Assert.LessOrEqual(allocAfter - allocBefore, 0);
+        [Test]
+        public void IsCoroutineActive_ShouldReturnTrueForActiveCoroutine()
+        {
+            var handle = Timing.RunCoroutine(SimpleCoroutine());
+            Assert.IsTrue(Timing.IsCoroutineActive(handle.Version));
+        }
+
+        [Test]
+        public void IsCoroutineActive_ShouldReturnFalseForKilledCoroutine()
+        {
+            var handle = Timing.RunCoroutine(SimpleCoroutine());
+            Timing.KillCoroutine(handle.Version);
+            Assert.IsFalse(Timing.IsCoroutineActive(handle.Version));
+        }
+
+        [Test]
+        public void PauseCoroutine_ShouldPauseCoroutine()
+        {
+            var handle = Timing.RunCoroutine(SimpleCoroutine());
+            Timing.PauseCoroutine(handle.Version);
+
+            // Access internal state to verify paused (reflection or friend class if needed)
+            // Here, we check that the coroutine is still active but will not advance
+            Assert.IsTrue(Timing.IsCoroutineActive(handle.Version));
+        }
+
+        [Test]
+        public void ResumeCoroutine_ShouldResumePausedCoroutine()
+        {
+            var handle = Timing.RunCoroutine(SimpleCoroutine());
+            Timing.PauseCoroutine(handle.Version);
+            Timing.ResumeCoroutine(handle.Version);
+
+            Assert.IsTrue(Timing.IsCoroutineActive(handle.Version));
+        }
+
+        [Test]
+        public void KillCoroutine_ShouldRemoveCoroutine()
+        {
+            var handle = Timing.RunCoroutine(SimpleCoroutine());
+            Timing.KillCoroutine(handle.Version);
+            Assert.IsFalse(Timing.IsCoroutineActive(handle.Version));
+        }
+
+        [Test]
+        public void KillAllCoroutines_ShouldRemoveAllCoroutines()
+        {
+            var handle1 = Timing.RunCoroutine(SimpleCoroutine());
+            var handle2 = Timing.RunCoroutine(SimpleCoroutine(), Segment.FixedUpdate);
+
+            Timing.KillAllCoroutines();
+
+            Assert.IsFalse(Timing.IsCoroutineActive(handle1.Version));
+            Assert.IsFalse(Timing.IsCoroutineActive(handle2.Version));
+        }
+
+        [Test]
+        public void KillAllCoroutines_BySegment_ShouldRemoveOnlyThatSegment()
+        {
+            var handle1 = Timing.RunCoroutine(SimpleCoroutine(), Segment.Update);
+            var handle2 = Timing.RunCoroutine(SimpleCoroutine(), Segment.FixedUpdate);
+
+            Timing.KillAllCoroutines((int)Segment.Update);
+
+            Assert.IsFalse(Timing.IsCoroutineActive(handle1.Version));
+            Assert.IsTrue(Timing.IsCoroutineActive(handle2.Version));
+        }
+
+        [Test]
+        public void ProcessSegment_ShouldAdvanceCoroutine()
+        {
+            // Set up a coroutine that increments a counter
+            int counter = 0;
+            IEnumerator<float> CountingCoroutine()
+            {
+                counter++;
+                yield return 0;
+                counter++;
+            }
+
+            var handle = Timing.RunCoroutine(CountingCoroutine(), Segment.Update);
+
+            // Call Update (which calls ProcessSegment)
+            Timing.Instance.Update();
+
+            Assert.AreEqual(2, counter); // Should have advanced both steps
+        }
+
+        [Test]
+        public void OnDestroy_ShouldKillAllCoroutines()
+        {
+            var handle = Timing.RunCoroutine(SimpleCoroutine());
+            Timing.Instance.OnDestroy();
+            Assert.IsFalse(Timing.IsCoroutineActive(handle.Version));
+        }
+
+        [Test]
+        public void Awake_ShouldInitializePools()
+        {
+            Timing.Instance.Awake();
+
+            var processPool = Timing.Instance.ProcessPool;
+            var handlePool = Timing.Instance.HandlePool;
+
+            Assert.IsNotNull(processPool);
+            Assert.IsNotNull(handlePool);
+        }
+
+        [UnityTest]
+        public IEnumerator ZeroAllocationCheck()
+        {
+            GC.Collect();
+            var allocBefore = UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var handle = Timing.RunCoroutine(SimpleCoroutine());
+                yield return null;
+            }
+
+            var allocAfter = UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong();
+            Assert.LessOrEqual(allocAfter - allocBefore, 0);
+        }
     }
 }
